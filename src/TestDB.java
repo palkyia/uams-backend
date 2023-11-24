27ac643a40435847fdcaf80a9a8cdde86f9e5e20
Sneupi <gabevenegas527@gmail.com>
2023-11-23 22:29:37 -0700

    split testSaveGetApplication() into testSaveApplication(), testGetApplication()

--- src/TestDB.java
+++ src/TestDB.java
@@ -11,7 +11,8 @@ public class TestDB {
     public static void main(String[] args) {
         UamsDAO uamsDAO = new UamsDAO();
         testLogin(uamsDAO);
-        testSaveGetApplication(uamsDAO);
+        testSaveApplication(uamsDAO);
+        testGetApplication(uamsDAO);
 
     }
 
@@ -27,7 +28,7 @@ public class TestDB {
      *
      * @param uamsDAO
      */
-    public static void testSaveGetApplication(UamsDAO uamsDAO) {
+    public static void testSaveApplication(UamsDAO uamsDAO) {
         // login to existing user
         UUID sessionID = uamsDAO.loginWithSecurityAnswer("jphan07", "lol", "What is your mother's maiden name?", "Phan");
         if (sessionID == null) {
@@ -35,16 +36,17 @@ public class TestDB {
             return;
         }
 
-        // get an existing scholarship_form's uuid
-        UUID scholarshipID = UUID.fromString("118c6269-5dbf-4efd-9e28-c178dd887319"); //TODO: Unsafe. Replace w/ a uamsDAO.getScholarship().getScholarshipID() function
-
         // get an existing local PC file "testfile.txt" on local desktop
         String desktopPath = System.getProperty("user.home") + "\\Desktop";
         File testfile = new File(desktopPath, "testfile.txt");
+        if (!testfile.exists()) {
+            System.out.println("testfile.txt does not exist on local PC");
+            return;
+        }
 
         // create a test application
         String[] responses = {"response1", "lol2", "something3", "1a", "2b", "3c", "lmao", "4d", "5e", "6f"};
-        Application application = new Application("jphan07", scholarshipID, responses, testfile);
+        Application application = new Application("jphan07", UUID.fromString("118c6269-5dbf-4efd-9e28-c178dd887319"), responses, testfile);
 
         // attempt saving
         boolean isSaved = uamsDAO.saveApplication(sessionID, application);
@@ -53,20 +55,29 @@ public class TestDB {
             return;
         }
 
-        // attempt getting
-        Application application_got = uamsDAO.getApplication(sessionID, "jphan07", scholarshipID);
+
+    }
+
+    public static void testGetApplication(UamsDAO uamsDAO) {
+        // login to existing user
+        UUID sessionID = uamsDAO.loginWithSecurityAnswer("jphan07", "lol", "What is your mother's maiden name?", "Phan");
+        if (sessionID == null) {
+            System.out.println("User not in db.");
+            return;
+        }
+
+        // get existing application
+        Application application_got = uamsDAO.getApplication(sessionID, "jphan07", UUID.fromString("118c6269-5dbf-4efd-9e28-c178dd887319"));
         if (application_got == null) {
             System.out.println("Failed to get application.");
             return;
         }
-
 
         //print application_got
         System.out.println("\nScholarshipID: %s".formatted(application_got.getScholarshipID()));
         System.out.println("StudentUsername: %s".formatted(application_got.getUsername()));
         System.out.println("Responses: %s".formatted(String.join(", ", application_got.getResponses())));
         System.out.println("PathToFileUpload: %s".formatted(application_got.getUploadedFilePath()));
-
     }
 
 }
